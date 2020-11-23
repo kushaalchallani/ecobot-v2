75fa2aeaba5e656efb46741ecdb98ffe55b2690a
Kushaal Challani <66863823+kcplayz@users.noreply.github.com>
2020-11-23 11:21:06 +0530

    Added ready event

---
+++ src/events/ready.js
@@ -0,0 +1,26 @@
+const Event = require("../structures/bases/eventBase");
+
+module.exports = class extends Event {
+    constructor(...args) {
+        super(...args, {
+            name: "ready",
+            once: true,
+        });
+    }
+
+    async execute() {
+        console.log(this.client.user.tag + " is now online.");
+        console.log(this.client.user.tag + " is in " + this.client.guilds.cache.size + " servers!");
+
+        const statuses = [
+            { name: "eb!help | @EcoBot for more Info", options: { type: "PLAYING" } },
+            { name: `eb!help | ${this.client.users.cache.size} users`, options: { type: "PLAYING" } },
+            { name: `eb!help | ${this.client.guilds.cache.size} servers`, options: { type: "PLAYING" } },
+        ];
+
+        setInterval(() => {
+            const status = statuses[Math.floor(Math.random() * statuses.length)];
+            this.client.user.setActivity(status.name, status.options);
+        }, 20000);
+    }
+};
