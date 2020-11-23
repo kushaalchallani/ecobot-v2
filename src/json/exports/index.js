6ebe970f39ae7966e459799bf30b7b083beb9a4b
Kushaal Challani <66863823+kcplayz@users.noreply.github.com>
2020-11-23 11:16:51 +0530

    Added JSON files

---
+++ src/json/exports/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+    actions: require("../actions.json"),
+    colors: require("../colors.json"),
+    errormsgs: require("../errors.json"),
+    filterlevels: require("../filterlevel.json"),
+    flags: require("../flags.json"),
+    fortune: require("../fortune.json"),
+    pp: require("../pp.json"),
+    region: require("../region.json"),
+    replies: require("../replies.json"),
+    verification: require("../verification.json"),
+    nsfw: require("../nsfw.json"),
+};
