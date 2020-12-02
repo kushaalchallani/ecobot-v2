b7d119d71508d82c80004b77fcb703ce129f8900
Kushaal Challani <66863823+kcplayz@users.noreply.github.com>
2020-12-02 15:07:08 +0530

    Added Daily Cmd & Using yarn

---
+++ src/commands/Economy/daily.js
@@ -0,0 +1,46 @@
+const Command = require("../../structures/bases/commandBase");
+const prettyMilliseconds = require("pretty-ms");
+const Embed = require("../../structures/embed");
+
+module.exports = class extends (
+    Command
+) {
+    constructor(...args) {
+        super(...args, {
+            name: "daily",
+            description: "Search discord api documentation.",
+            category: "Bot Owner",
+            botPermission: ["SEND_MESSAGES", "EMBED_LINKS"],
+            memberPermission: ["SEND_MESSAGES"],
+            nsfw: false,
+            cooldown: 10,
+            examples: ["docs Client", "docs Message", "docs ClientUser#setActivity --src=master"],
+        });
+    }
+
+    async execute(message, args) {
+        let user = await bot.fetchUser(message.author.id);
+        if (Date.parse(user.dailyStreak) + 86400000 > Date.now()) {
+            const embed = new MessageEmbed()
+                .setTitle("Cooldown")
+                .setDescription(
+                    `Woah there, you need to wait \`${prettyMilliseconds(
+                        Date.parse(user.dailyStreak) + 86400000 - Date.now()
+                    )}\` before using this command again.
+                
+                The default cooldown on this command is \`1d\``
+                )
+                .setColor(0x3c54b4);
+            return message.channel.send(embed);
+        } else {
+            user.dailyStreak = new Date(Date.now());
+            user.coinsInWallet += 1000;
+            await user.save();
+            const claimed = new MessageEmbed()
+                .setTitle("Gave you 1000 coins.")
+                .setDescription("Use this command in `24h` to claim your daily reward again!")
+                .setColor("RANDOM");
+            message.channel.send(claimed);
+        }
+    }
+};
