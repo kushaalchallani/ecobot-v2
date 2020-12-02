6813b0f469d5601303e214c0fca15c26e31581fb
Kushaal Challani <66863823+kcplayz@users.noreply.github.com>
2020-12-02 15:43:29 +0530

    Added a sell cmd

---
+++ src/commands/Economy/sell.js
@@ -0,0 +1,90 @@
+const Command = require("../../structures/bases/commandBase");
+const itemss = require("../../structures/items");
+const Embed = require("../../structures/embed");
+
+module.exports = class extends Command {
+    constructor(...args) {
+        super(...args, {
+            name: "sell",
+            description: "Search discord api documentation.",
+            category: "Bot Owner",
+            botPermission: ["SEND_MESSAGES", "EMBED_LINKS"],
+            memberPermission: ["SEND_MESSAGES"],
+            nsfw: false,
+            cooldown: 10,
+            examples: ["docs Client", "docs Message", "docs ClientUser#setActivity --src=master"],
+        });
+    }
+
+    async execute(message, args) {
+        const user = await this.client.fetchUser(message.author.id);
+        if (!args.join(" ")) {
+            return message.channel.send("you can't sell nothing lmao");
+        }
+        if (!args[1]) args[1] = "";
+        const item = itemss.find(
+            (x) =>
+                x.name.toLowerCase() === args.join(" ").toString().toLowerCase() ||
+                x.name.toLowerCase() === args[0].toString().toLowerCase() ||
+                x.name.toLowerCase() === `${args[0].toString().toLowerCase()} ${args[1].toString().toLowerCase()}`
+        );
+        let sellAmount = args
+            .join(" ")
+            .toString()
+            .match(/([1-9][0-9]*)/);
+        if (!sellAmount) sellAmount = 1;
+        else sellAmount = sellAmount[0];
+        if (!item) {
+            return message.channel.send("can't sell this item");
+        }
+        const founditem = user.items.find((x) => x.name.toLowerCase() === item.name.toLowerCase());
+        let array = [];
+        array = user.items.filter((x) => x.name !== item.name);
+        if (!founditem) {
+            return message.channel.send("you don't have this item");
+        }
+        if (args[1] == "all" || args[2] == "all") {
+            sellAmount = Math.floor(founditem.amount * item.sellAmount);
+            user.items = array;
+            user.coinsInWallet += sellAmount;
+            user.save();
+            const embed = new Embed()
+                .setAuthor("Sold")
+                .setDescription(
+                    `You sold ${parseInt(sellAmount / item.sellAmount).toLocaleString()} **${
+                        item.name
+                    }** for \`${sellAmount.toLocaleString()}\` coins`
+                )
+                .setColor("RANDOM");
+            return message.channel.send(embed);
+        }
+        if (founditem.amount < parseInt(sellAmount))
+            return message.channel.send(`You only have ${founditem.amount} of this item`);
+        if (founditem.amount === 1) {
+            user.items = array;
+            await user.save();
+        } else if (founditem.amount - parseInt(sellAmount) == 0) {
+            user.items = array;
+            await user.save();
+        } else {
+            array.push({
+                name: item.name,
+                amount: founditem.amount - parseInt(sellAmount),
+                description: item.description,
+            });
+            user.items = array;
+            await user.save();
+        }
+        user.coinsInWallet += item.sellAmount * parseInt(sellAmount);
+        await user.save();
+        const embed = new Embed()
+            .setAuthor("Sold")
+            .setDescription(
+                `You sold ${parseInt(sellAmount).toLocaleString()} **${item.name}** for \`${parseInt(
+                    item.sellAmount * sellAmount
+                ).toLocaleString()}\` coins`
+            )
+            .setColor("RANDOM");
+        message.channel.send(embed);
+    }
+};
