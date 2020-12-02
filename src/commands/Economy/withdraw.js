cfc65065aced0950d574b053118c9e60fbe8af28
Kushaal Challani <66863823+kcplayz@users.noreply.github.com>
2020-12-02 15:51:57 +0530

    Create withdraw.js

---
+++ src/commands/Economy/withdraw.js
@@ -0,0 +1,46 @@
+const Command = require("../../structures/bases/commandBase");
+
+module.exports = class extends Command {
+    constructor(...args) {
+        super(...args, {
+            name: "withdraw",
+            description: "Search discord api documentation.",
+            category: "Bot Owner",
+            botPermission: ["SEND_MESSAGES", "EMBED_LINKS"],
+            memberPermission: ["SEND_MESSAGES"],
+            nsfw: false,
+            cooldown: 10,
+            examples: ["docs Client", "docs Message", "docs ClientUser#setActivity --src=master"],
+        });
+    }
+
+    async execute(message, args) {
+        const data = await this.client.fetchUser(message.author.id);
+
+        if (args.join(" ") === "all" || args.join(" ") === "max") {
+            data.coinsInWallet += data.coinsInBank;
+
+            await message.channel.send(`Withdrawed **${data.coinsInBank}** coins.`);
+
+            data.coinsInBank -= data.coinsInBank;
+
+            await data.save();
+        } else {
+            if (isNaN(args[0])) {
+                return message.channel.send("That's not a number.");
+            }
+
+            if (parseInt(args[0]) > data.coinsInBank) {
+                return message.channel.send("You do not have that much coins.");
+            }
+
+            data.coinsInWallet += parseInt(args[0]);
+
+            await message.channel.send(`Withdrawed **${args[0]}** coins.`);
+
+            data.coinsInBank -= parseInt(args[0]);
+
+            await data.save();
+        }
+    }
+};
