3cdb162782de4d8a16edce5a0ba38983d56e6f26
Kushaal Challani <66863823+kcplayz@users.noreply.github.com>
2020-12-03 11:12:16 +0530

    Moved the eco function to util

--- src/structures/client.js
+++ src/structures/client.js
@@ -3,8 +3,8 @@ const { Client, Collection } = require("discord.js");
 const { eventRegistry, commandRegistry } = require("../registries/export/index");
 const Util = require("./util");
 const { suggestion, afk, thanklb } = require("../features/exports/index");
-const { ecoGuildModel, economyModel } = require("../database/models/export/index");
 const { itemManager } = require("../utils/export/index");
+const { economyModel } = require("../database/models/export/index");
 
 module.exports = class EcoBot extends Client {
     constructor() {
@@ -41,109 +41,5 @@ module.exports = class EcoBot extends Client {
         afk(this);
         require("../database/database")();
         super.login(process.env.BOT_TOKEN);
-    }
-
-    // -----------------------------------------------------------------------------------------------------------------------------
-    // --------------------------------------------------Economy System------------------------------------------------------------
-    // -----------------------------------------------------------------------------------------------------------------------------
-
-    /**
-     *
-     * @param {string} userId - A discord user ID.
-     */
-
-    async fetchUser(userId) {
-        const someone = this.users.cache.get(userId);
-        if (!someone || someone.bot) return false;
-        const user = await economyModel.findOne({ userId: userId });
-        if (!user) {
-            const newUser = new economyModel({
-                userId: userId,
-                items: [],
-            });
-            newUser.save();
-            return newUser;
-        }
-        return user;
-    }
-
-    /**
-     *
-     * @param {string} userId - A discord user ID.
-     * @param {number} amount - Amount of bank space to give.
-     */
-
-    async giveBankSpace(userId, amount) {
-        const someone = this.users.cache.get(userId);
-        if (!someone || someone.bot) return false;
-        const user = await economyModel.findOne({ userId: userId });
-        if (!user) {
-            const newUser = new economyModel({
-                userId: userId,
-                items: [],
-            });
-            newUser.save();
-            return newUser;
-        }
-        user.bankSpace += parseInt(amount);
-        await user.save();
-        return user;
-    }
-
-    /**
-     *
-     * @param {string} userId - A discord user ID.
-     */
-
-    async createUser(userId) {
-        const someone = this.users.cache.get(userId);
-        if (!someone || someone.bot) return false;
-        const user = await economyModel.findOne({ userId: userId });
-        if (!user) return false;
-        const newUser = new economyModel({
-            userId: userId,
-            items: [],
-        });
-        newUser.save();
-        return newUser;
-    }
-
-    /**
-     *
-     * @param {string} userId - A discord user ID.
-     * @param {number} amount - Amount of coins to give.
-     */
-
-    async giveCoins(userId, amount) {
-        const someone = this.users.cache.get(userId);
-        if (!someone || someone.bot) return false;
-        const user = await economyModel.findOne({ userId: userId });
-        if (!user) {
-            const newUser = new economyModel({
-                userId: userId,
-                items: [],
-                coinsInWallet: parseInt(amount),
-            });
-            newUser.save();
-            return newUser;
-        }
-        user.coinsInWallet += parseInt(amount);
-        await user.save();
-        return user;
-    }
-
-    async fetchGuild(guildID) {
-        const guild = this.guilds.cache.get(guildID);
-        if (!guild) return undefined;
-        const config = await ecoGuildModel.findOne({ guildId: guildID });
-        if (!config) {
-            const newConfig = new ecoGuildModel({
-                guildId: guildID,
-            });
-            await newConfig.save();
-            return newConfig;
-        } else {
-            return config;
-        }
     }
 };
