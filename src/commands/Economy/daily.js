f7056707a171b103d91dbc1769b8e85c3957f010
Kushaal Challani <66863823+kcplayz@users.noreply.github.com>
2020-12-03 14:38:30 +0530

    Changed Text and amount of money to give

--- src/commands/Economy/daily.js
+++ src/commands/Economy/daily.js
@@ -1,29 +1,33 @@
 const Command = require("../../structures/bases/commandBase");
 const prettyMilliseconds = require("pretty-ms");
 const Embed = require("../../structures/embed");
+const { prefixModel } = require("../../database/models/export/index");
 
 module.exports = class extends Command {
     constructor(...args) {
         super(...args, {
             name: "daily",
-            description: "Search discord api documentation.",
-            category: "Bot Owner",
+            description: "Get your Daily coins and spend em!",
+            category: "Economy",
             botPermission: ["SEND_MESSAGES", "EMBED_LINKS"],
             memberPermission: ["SEND_MESSAGES"],
             nsfw: false,
-            cooldown: 10,
+            premium: false,
             bankSpace: 0,
-            examples: ["docs Client", "docs Message", "docs ClientUser#setActivity --src=master"],
         });
     }
 
     async execute(message) {
-        const user = await this.client.fetchUser(message.author.id);
+        const prefix = (await prefixModel.findOne({ guildID: message.guild.id }))
+            ? (await prefixModel.findOne({ guildID: message.guild.id })).prefix
+            : this.client.prefix;
+
+        const user = await this.client.util.fetchUser(message.author.id);
         if (Date.parse(user.dailyStreak) + 86400000 > Date.now()) {
             const embed = new Embed()
-                .setTitle("Cooldown")
+                .setTitle("**Slow it down!**")
                 .setDescription(
-                    `Woah there, I cant give you so much money \`${prettyMilliseconds(
+                    `Woah there, I cant give you so much money. Wait \`${prettyMilliseconds(
                         Date.parse(user.dailyStreak) + 86400000 - Date.now()
                     )}\` before using this command again.
                 
@@ -33,11 +37,13 @@ module.exports = class extends Command {
             return message.channel.send(embed);
         } else {
             user.dailyStreak = new Date(Date.now());
-            user.coinsInWallet += 1000;
+            user.coinsInWallet += 2000;
             await user.save();
             const claimed = new Embed()
-                .setTitle("Gave you 1000 coins.")
-                .setDescription("Use this command in `24h` to claim your daily reward again!")
+                .setTitle(`Here are your daily coins, **${message.author.username}**`)
+                .setDescription(
+                    `**2000 Coins** were placed in your wallet\n\nYou can get your weekly and monthly coins by buying premium. Use \`${prefix}patreon\` and view the perks. [Click here](https://www.patreon.com/Ecoobot) to go patreon`
+                )
                 .setColor("RANDOM");
             message.channel.send(claimed);
         }
