7c01f20fad5190f1c2dbe5b96c9646a4a37caacf
Kushaal Challani <66863823+kcplayz@users.noreply.github.com>
2020-12-04 09:49:47 +0530

    Changed text to embed and updated cmd base

--- src/commands/Economy/inventory.js
+++ src/commands/Economy/inventory.js
@@ -1,23 +1,24 @@
 const Command = require("../../structures/bases/commandBase");
 const Embed = require("../../structures/embed");
+const { error } = require("../../utils/export/index");
 
 module.exports = class extends Command {
     constructor(...args) {
         super(...args, {
             name: "inventory",
-            description: "Search discord api documentation.",
-            category: "Bot Owner",
+            description: "Sends your inventory.",
+            category: "Economy",
             botPermission: ["SEND_MESSAGES", "EMBED_LINKS"],
             memberPermission: ["SEND_MESSAGES"],
             nsfw: false,
             cooldown: 10,
             bankSpace: 0,
-            examples: ["docs Client", "docs Message", "docs ClientUser#setActivity --src=master"],
+            aliases: ["inv"],
         });
     }
 
     async execute(message, args) {
-        const user = await this.client.fetchUser(message.author.id);
+        const user = await this.client.util.fetchUser(message.author.id);
         let number = 5 * parseInt(args[0]);
         let page;
         if (user.items.length <= 5) page = 1;
@@ -29,7 +30,7 @@ module.exports = class extends Command {
         }
         const item = user.items.slice(number - 5, number);
         if (item.length < 1) {
-            return message.channel.send("You have no items.");
+            return error("You have no items.", message.channel);
         }
         const items = item.map((x) => `**${x.name}** - ${x.amount.toLocaleString()}\n${x.description}`);
         const embed = new Embed()
