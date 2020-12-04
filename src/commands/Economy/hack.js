107b68c024098254d37065d1b2113aca63f840e0
Kushaal Challani <66863823+kcplayz@users.noreply.github.com>
2020-12-04 09:43:58 +0530

    Changed text to embed and the cmd base

--- src/commands/Economy/hack.js
+++ src/commands/Economy/hack.js
@@ -1,37 +1,39 @@
 const Command = require("../../structures/bases/commandBase");
+const { error, success } = require("../../utils/export/index");
 
 module.exports = class extends Command {
     constructor(...args) {
         super(...args, {
             name: "hack",
-            description: "Search discord api documentation.",
-            category: "Bot Owner",
+            description: "Hack a random member.",
+            category: "Economy",
             botPermission: ["SEND_MESSAGES", "EMBED_LINKS"],
             memberPermission: ["SEND_MESSAGES"],
             nsfw: false,
-            cooldown: 10,
             bankSpace: 5,
-            examples: ["docs Client", "docs Message", "docs ClientUser#setActivity --src=master"],
+            cooldown: 60,
+            aliases: ["randomrob"],
         });
     }
 
     async execute(message) {
-        const author = await this.client.fetchUser(message.author.id);
+        const author = await this.client.util.fetchUser(message.author.id);
+
         if (author.coinsInWallet < 200)
-            return message.channel.send("You need atleast **200** coins to use this command.");
+            return error("You need atleast **200** coins to use this command.", message.channel);
         if (message.guild.members.cache == undefined)
-            return message.channel.send("Seems like members here aren't cached.");
-        if (message.guild.memberCount < 2)
-            return message.channel.send("This server has less than 5 members, you can't use this command.");
+            return error("Seems like members here aren't cached.", message.channel);
+        if (message.guild.memberCount < 5)
+            return error("This server has less than 5 members, you can't use this command.", message.channel);
         const members = message.guild.members.cache
             .filter((x) => x.user.id != message.author.id && !x.user.bot)
             .array();
         const random = Math.floor(Math.random() * members.length);
         const memberPicked = members[random];
-        const target = await this.client.fetchUser(memberPicked.user.id);
+        const target = await this.client.util.fetchUser(memberPicked.user.id);
         const randomNumber = Math.floor(Math.random() * 100) + 100;
 
-        const msg = await message.channel.send("**Searching for a target to hack...**");
+        const msg = await success("**Searching for a target to hack...**", message.channel);
 
         setTimeout(async () => {
             if (target.coinsInWallet < randomNumber) {
@@ -41,8 +43,9 @@ module.exports = class extends Command {
                 await author.save();
                 await target.save();
                 msg.delete();
-                message.channel.send(
-                    `Turns out **${memberPicked.user.tag}** Didn't have much coins so you end up paying them **${randomNumber}** coins...`
+                success(
+                    `Turns out **${memberPicked.user.tag}** Didn't have much coins so you end up paying them **${randomNumber}** coins...`,
+                    message.channel
                 );
             } else {
                 author.coinsInWallet += randomNumber;
@@ -51,7 +54,10 @@ module.exports = class extends Command {
                 await author.save();
                 await target.save();
                 msg.delete();
-                message.channel.send(`You hacked **${memberPicked.user.tag}** and gained **${randomNumber}** coins.`);
+                success(
+                    `You hacked **${memberPicked.user.tag}** and gained **${randomNumber}** coins.`,
+                    message.channel
+                );
             }
         }, 5000);
         return true;
