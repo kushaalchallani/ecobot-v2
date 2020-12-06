09a5f1eb0b6707646bf5fbdfd872b0df044db39b
Kushaal Challani <66863823+kcplayz@users.noreply.github.com>
2020-12-06 15:17:35 +0530

    channelCreate event added

---
+++ src/events/channel/channelCreate.js
@@ -0,0 +1,61 @@
+require("dotenv/config");
+const Event = require("../../structures/bases/eventBase");
+const Embed = require("../../structures/embed");
+const { channellogsModel } = require("../../database/models/export/index");
+
+module.exports = class extends Event {
+    constructor(...args) {
+        super(...args, {
+            name: "channelCreate",
+        });
+    }
+
+    async execute(channel) {
+        const data = await channellogsModel.findOne({
+            guildId: channel.guild.id,
+        });
+
+        if (!data) return;
+
+        const sendchannel = channel.guild.channels.cache.find((channel) => channel.id === data.channelId);
+
+        try {
+            const fetchLogs = await channel.guild.fetchAuditLogs({
+                limit: 1,
+                type: "CHANNEL_CREATE",
+            });
+
+            const log = fetchLogs.entries.first();
+
+            const { executor } = log;
+
+            if (channel.type === "text") {
+                sendchannel.send(
+                    new Embed()
+                        .setColor("#00FF00")
+                        .setAuthor(
+                            `${executor.username}#${executor.discriminator} (${executor.id})`,
+                            executor.avatarURL({ dynamic: true })
+                        )
+                        .setDescription(`Text Channel \`${channel.name}\` has been created`)
+                        .setTimestamp(Date.now())
+                );
+            }
+
+            if (channel.type === "news") {
+                sendchannel.send(
+                    new Embed()
+                        .setColor("#00FF00")
+                        .setAuthor(
+                            `${executor.username}#${executor.discriminator} (${executor.id})`,
+                            executor.avatarURL({ dynamic: true })
+                        )
+                        .setDescription(`Announcement Channel \`${channel.name}\` has been created`)
+                        .setTimestamp(Date.now())
+                );
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    }
+};
