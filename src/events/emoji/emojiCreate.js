3155bbe91c84c1a475e2c65a14a54530c1b95003
Kushaal Challani <66863823+kcplayz@users.noreply.github.com>
2020-12-07 12:18:54 +0530

    Added emojiCreate event

---
+++ src/events/emoji/emojiCreate.js
@@ -0,0 +1,75 @@
+require("dotenv/config");
+const Event = require("../../structures/bases/eventBase");
+const Embed = require("../../structures/embed");
+const { serverlogsModel } = require("../../database/models/export/index");
+
+module.exports = class extends Event {
+    constructor(...args) {
+        super(...args, {
+            name: "emojiCreate",
+        });
+    }
+
+    async execute(emoji) {
+        const data = await serverlogsModel.findOne({
+            guildId: emoji.guild.id,
+        });
+
+        if (!data) return;
+
+        const channel = emoji.guild.channels.cache.find((channel) => channel.id === data.channelId);
+
+
+        if (!channel) return;
+
+        try {
+            const fetchLogs = await emoji.guild.fetchAuditLogs({
+                limit: 1,
+                type: "EMOJI_CREATE",
+            });
+
+            const log = fetchLogs.entries.first();
+
+            const { executor } = log;
+
+            if(emoji.animated) {
+                channel.send(
+                    new Embed()
+                        .setColor("#FFFF00")
+                        .setAuthor(
+                            `${executor.username}#${executor.discriminator} (${executor.id})`,
+                            executor.avatarURL({ dynamic: true })
+                        )
+                        .setDescription(`
+                        **Emoji Name:** ${emoji.name}
+                        **Emoji ID:** ${emoji.id}
+                        `
+                        )
+                        .setTitle("Emoji Created")
+                        .setURL(emoji.url)
+                        .setThumbnail(emoji.url)
+                        .setTimestamp(Date.now())
+                );
+            } else {
+                channel.send(
+                    new Embed()
+                        .setColor("#FFFF00")
+                        .setAuthor(
+                            `${executor.username}#${executor.discriminator} (${executor.id})`,
+                            executor.avatarURL({ dynamic: true })
+                        )
+                        .setDescription(
+                        `Emoji ${emoji.name}(${emoji.id}) has been created`
+                        )
+                        .setTitle("Emoji Created")
+                        .setURL(emoji.url)
+                        .setTimestamp(Date.now())
+                );
+            }
+
+            
+        } catch (err) {
+            console.log(err);
+        }
+    }
+};
